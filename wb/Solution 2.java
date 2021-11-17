24d3512151605df7e385c5b60ad6df506d4ae80b
wangbin <wangbin78@meituan.com>
2021-11-17 20:54:20 +0800

    目标和（背包题解）

---
+++ wb/Solution 2.java
@@ -0,0 +1,33 @@
+package com.example.lc;
+
+/**
+ * @ClassName: Solution
+ * @Description: //TODO
+ * @Author: wangbin
+ * @Date: 11/17/21
+ */
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int n = nums.length, neg = diff / 2;
+        int[][] dp = new int[n + 1][neg + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int num = nums[i - 1];
+            for (int j = 0; j <= neg; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= num) {
+                    dp[i][j] += dp[i - 1][j - num];
+                }
+            }
+        }
+        return dp[n][neg];
+    }
+}
